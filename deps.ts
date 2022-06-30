8482d369d303a908aae1e84d5238c171eb2e10bf
Hugo Tunius <hugo@lookback.io>
2022-06-30 17:59:24 +0100

    Fix loading from ~/.lbstatus

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export * as args from 'https://deno.land/x/args@2.1.1/index.ts';
 export * as colors from 'https://deno.land/std@0.143.0/fmt/colors.ts';
 export * as flags from 'https://deno.land/std@0.143.0/flags/mod.ts';
+export * as path from 'https://deno.land/std@0.143.0/path/mod.ts';
